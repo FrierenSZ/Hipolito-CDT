72e3f14b894e0d9c123899cbab8b0fb62cc04998
Hipolito <ghipolitomonteiro@gmail.com>
2025-10-13 17:00:12 -0300

    upload

---
+++ modulo_02/mensagem_boas_vindas.py
@@ -0,0 +1,31 @@
+'''
+Pedir para o usuário escrever o nome e idade
+
+'''
+
+print("\n---------------------------")
+print("Bem Vindo a tela!")
+print("\n---------------------------")
+
+nome = input('Qual seu nome? ')
+idade = int(input("Qual sua idade? "))
+
+
+print(f'Olá {nome}, Você tem {idade} anos de idade ')
+
+
+if idade >= 18:
+    print("Aparentemente você é maior de idade ")
+else:
+    print("Aparentemente você é menor de idade ")
+
+
+
+nascimento = (2025 - idade)
+print(f'Você nasceu no ano de {nascimento} ') 
+
+
+
+altura_pessoa = str(input("Qual sua altura? "))
+print('Exemplo: 1.70')
+print(f'Você tem {altura_pessoa} de altura! ')
